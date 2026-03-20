1102bcfac2e1a518a68c55bd88c22f1956800bdd
nithyasrik24 <nithyasrikesavan@gmail.com>
2026-03-20 23:09:48 +0530

    fix server start final

--- blockchain.py
+++ blockchain.py
@@ -147,6 +147,5 @@ def get_api(index):
 def home():
     return "Server is running"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT",5000))
+app.run(host="0.0.0.0", port=port)
